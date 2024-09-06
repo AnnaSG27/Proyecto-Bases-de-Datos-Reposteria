18fd66ebbab84cd65ebcdad5e5b8d3e9c1297f4a
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-09-06 17:18:15 -0500

    Add files via upload

---
+++ Reposteria.Categoria.sql
@@ -0,0 +1,5 @@
+SELECT * FROM reposteria.categoria;
+
+INSERT INTO Categoria(IDCategoria, NombreCategoria, Descripcion)
+		VALUES(01, 'Torta', 'Bizcocho de diferente sabor, tamaño y porción'),
+			  (02,'Cupcake', 'Bizcocho pequeño de una sola porción y de diferentes sabor');
---
+++ Reposteria.Usuario.sql
@@ -0,0 +1,5 @@
+SELECT * FROM reposteria.usuario;
+
+INSERT INTO Usuario(IDUsuario, Nombres, Apellidos, Telefono, Pais, Ciudad)
+		VALUES(01, 'Maria Fernanda', 'Álvarez Marín', '3243493284', 'Colombia', 'Medellín'),
+			  (02, 'Anna Sofía', 'Giraldo Carvajal', '3113750800', 'Colombia', 'Medellín');
