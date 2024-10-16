9dc2d649e5245788eba73d056ea9394db2279a7e
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-10-16 10:39:32 -0500

    Add files via upload

---
+++ Reposteria.Proveedores.sql
@@ -0,0 +1,6 @@
+SELECT * FROM reposteria.proveedores;
+
+INSERT INTO Proveedores(IDProveedor, NombreEmpresa, NombreContacto, CorreoContacto, TelefonoCOntacto, Direccion, Ciudad, Pais, TipoProductos, TiempoEntrega, FechaInicioContrato)
+		VALUES (1, 'Mundo Huevo', 'Lina Carvajal', 'mundohuevo123@gmail.com', '3147292933', 'X', 'Envigado', 'Colombia', 'Todo tipo de Productos', 1, '2024-10-15'),
+			   (2, 'Colanta', 'Lina Carvajal', 'colanta123@gmail.com', '3147292933', 'X', 'Medellín', 'Colombia', 'Lácteos', 1, '2024-10-15');
+        
