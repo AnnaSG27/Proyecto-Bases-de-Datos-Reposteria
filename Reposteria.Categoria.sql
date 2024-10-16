376ab56cff2bd43d963a26157eb337e71863c02a
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-10-16 10:05:25 -0500

    Update Reposteria.Categoria.sql

--- Reposteria.Categoria.sql
+++ Reposteria.Categoria.sql
@@ -2,4 +2,4 @@ SELECT * FROM reposteria.categoria;
 
 INSERT INTO Categoria(IDCategoria, NombreCategoria, Descripcion)
 		VALUES(01, 'Torta', 'Bizcocho de diferente sabor, tamaño y porción'),
-			  (02,'Cupcake', 'Bizcocho pequeño de una sola porción y de diferentes sabor');
+		      (02,'Cupcake', 'Bizcocho pequeño de una sola porción y de diferentes sabor');
