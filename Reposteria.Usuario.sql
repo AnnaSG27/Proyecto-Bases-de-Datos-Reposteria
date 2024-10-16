b6eab32cf8c3e2191469dffe8cd52ca34db6d41e
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-10-16 10:04:04 -0500

    Update Reposteria.Usuario.sql

--- Reposteria.Usuario.sql
+++ Reposteria.Usuario.sql
@@ -1,5 +1,5 @@
 SELECT * FROM reposteria.usuario;
 
-INSERT INTO Usuario(IDUsuario, Nombres, Apellidos, Telefono, Pais, Ciudad)
-		VALUES(01, 'Maria Fernanda', 'Álvarez Marín', '3243493284', 'Colombia', 'Medellín'),
-			  (02, 'Anna Sofía', 'Giraldo Carvajal', '3113750800', 'Colombia', 'Medellín');
+INSERT INTO Usuario(IDUsuario, Nombres, Apellidos, Correo, Telefono, Direccion, Pais, Ciudad, CodigoPostal, FechaNacimiento, FechaCumpleaños, Username, Password)
+		VALUES(01, 'Maria Fernanda', 'Álvarez Marín', 'mariamariaferalvarez0426@gmail.com', '3243493284', 'X', 'Colombia', 'Medellín', 050031, '2005-04-26', '2024-04-26', 'MafeAlvarez', 'jYzxjhty198'),
+		      (02, 'Anna Sofía', 'Giraldo Carvajal', 'annagica0207@gmail.com', '3113750800', 'Calle 2B', 'Colombia', 'Medellín', 050026, '2004-07-02', '2024-07-02', 'AnnaG', 'asgc0027');
