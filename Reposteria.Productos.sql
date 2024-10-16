fe0cc7b8e104cc63267921d16c89b4d03209fa65
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-10-16 10:04:53 -0500

    Update Reposteria.Productos.sql

--- Reposteria.Productos.sql
+++ Reposteria.Productos.sql
@@ -2,4 +2,4 @@ SELECT * FROM reposteria.productos;
 
 INSERT INTO Productos(IDProducto, NombreProducto, DescripcionProducto, Costo, IDCategoria, Porciones, Personalizable, Disponible, CantidadesVendidas, Ingredientes, PrecioProducto)
 			VALUES(01, 'Torta de Chocolate', 'X', 2500, 01, '1 Porción', FALSE, TRUE, 0, 'X', 5000 ),
-				  (02, 'Cupcake de Vainilla', 'X', 2000, 02, '1 Porción', TRUE, TRUE, 0, 'X', 4000 );
+			      (02, 'Cupcake de Vainilla', 'X', 2000, 02, '1 Porción', TRUE, TRUE, 0, 'X', 4000 );
